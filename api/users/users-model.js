97164bb768eb7c6b39402c529211515f2b88b8b9
Bobby <monkeyeden.rjt@gmail.com>
2021-08-16 18:33:20 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,14 +1,17 @@
+const db = require('../../data/db-config')
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db('users').select('user_id','username')
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
+  return db('users').where()
 
 }
 
