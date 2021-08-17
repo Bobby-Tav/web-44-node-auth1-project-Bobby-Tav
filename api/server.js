70c14e142f76b4103b93b1b9e5c2a159ab952672
Bobby <monkeyeden.rjt@gmail.com>
2021-08-16 20:27:13 -0400

    wip

--- api/server.js
+++ api/server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const userRouter = require('../api/users/users-router')
 const authRouter = require('../api/auth/auth-router')
 const session = require("express-session")
-const store = require('connect-session-knex')(session)
+const Store = require('connect-session-knex')(session)
+const knex = require('./../data/db-config')
 
 
 
@@ -27,8 +28,20 @@ const server = express();
 server.use(session({
   name:'chocolatechip',
   secret:'no clue',
+  saveUninitialized:false,
+  resave:false,
+  store: new Store({
+    knex,
+    createTable:true,
+    clearInterval:1000*60*10,
+    tablename:'session',
+    sidfieldname:'sid'
+  })
   cookie:{
-    maxAge: 1000* 60 * 60
+    maxAge: 1000* 60 * 60,
+    secure:false,
+    httpOnly:true,
+    // sameSite:'none'
   }
 }))
 server.use(helmet());
